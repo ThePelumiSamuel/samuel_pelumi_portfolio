7e74fb62cc4c8742305b4f0cab1831a1e37ff411
pelly <adesanyapelumi2017@mail.com>
2025-06-16 01:00:21 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="font">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+
+
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet"> -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <title>Samuel pelumi portfolio</title>
+</head>
+
+<body>
+    <div id="root">
+        <header class="ds-header shadow-sm" id="site-header">
+            <div class="container">
+                <div class="ds-header-inner ">
+                    <a href="#" class="ds-logo">
+                        <span>SPA</span>
+                        Samuel pelumi Adesanya
+                    </a>
+                    <ul class="ds-social">
+                        <li>
+                            <a href="{https://github.com/ThePelumiSamuel/Agrolink.git" target="_blank" rel="">
+                                <i class="fa-brands fa-github" style="color: #ffffff;"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank" rel="">
+                                <i class="fa-brands fa-stack-overflow"></i>
+
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://linkedin.com/in/samuel-pelumi-adesanya-2686241a1" target="_blank" rel="">
+                                <i class="fa-brands fa-linkedin-in"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+
+
+        <div class="ds-banner" style="margin-top: 180px;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 col-xxl-5">
+                        <figure>
+                            <img src="images/profilepicture.jpg" alt="profile_image">
+                        </figure>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-7 col-lg-7 col-xl-7 col-xxl-7">
+                        <section>
+                            <h1>
+                                <span>HI THERE</span>
+                                I'm Samuel Pelumi Adesanya
+                                <br>
+                                FULL-STACK WEB DEVELOPER based in Lagos, Nigeria.
+                            </h1>
+                            <ul class="ds-numbervalulist">
+                                <li>
+                                    <strong>1 +</strong>
+                                    <span>Experience</span>
+                                </li>
+                                <li>
+                                    <strong>1</strong>
+                                    <span>Projects</span>
+                                </li>
+                                <li>
+                                    <strong>0</strong>
+                                    <span>Happy Clients</span>
+                                </li>
+                            </ul>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="ds-about-section">
+            <div class="container">
+                <section>
+                    <h2 class="ds-heading">
+                        About Me
+                    </h2>
+                    <p>I'm Samuel Pelumi Adesanya, a passionate and driven web developer with a Higher National Diploma in Computer Science from D.S Adegbenro ICT Polytechnic. My journey in tech began with a deep interest in solving real-world problems through code, which has led me to build creative and functional digital experiences.
+
+                        With hands-on experience gained through internships at OACO and PaySprnt, I’ve contributed to real projects ranging from revamping websites to collaborating with developers on dynamic features. As the CEO of Techive Group of Companies, I lead initiatives in web development, CCTV installation, solar energy, and computer hardware repair, helping individuals and businesses leverage technology for growth.
+
+                        I specialize in HTML, CSS, JavaScript, PHP, and Laravel, and I’m particularly focused on building solutions that create impact such as financial literacy apps, farm-to-customer platforms, and e-commerce tools.
+
+                        I’m also a God-loving individual, a student politician, and a firm believer in sustainable innovation and the future of brain-computer interfaces and neuromorphic computing.
+
+                        Let’s build something amazing together.</p>
+                    <div class="ds-button-sec text-center">
+                        <a href="pdf/mycv.pdf" class="ds-button" target="_blank" rel="noreferrer">
+                            Download Resume
+                        </a>
+                    </div>
+                </section>
+            </div>
+        </div>
+
+
+        <div class="ds-skills-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <h2 class="ds-heading">Core Skills</h2>
+                        <ul class="ds-skills-list">
+                            <li>Laravel</li>
+                            <li>JavaScript</li>
+                            <li>PHP</li>
+                            <li>Node.js</li>
+                            <li>MongoDB</li>
+                            <li>React</li>
+                            <li>Github</li>
+                            <li>HTML & CSS</li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <h2 class="ds-heading">Other Skills</h2>
+                        <ul class="ds-skills-list">
+                            <li>APIs</li>
+                            <li>Object Oriented Programming</li>
+                            <li>Googling</li>
+                            <li>Responsive Web Design</li>
+                            <li>Test Driven Development</li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="ds-experience-section">
+            <div class="container">
+                <h2 class="ds-heading">Work Experience</h2>
+                <div class="row ds-experience-list">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <section>
+                            <span class="ds-year">2021 - 2022</span>
+                            <h3 class="ds-officename">PaySprint Financial Tech. (Lagos)</h3>
+                            <span class="ds-department">Intenship</span>
+                            <p>PaySprint is a fin-tech company working on an application that allows users to take control of their bills including invoices and also makes it easy for user to perform a cross border transaction - <a href="https://paysprint.ca">https://paysprint.ca</a></p>
+                            <span class="ds-department">Job description and role</span>
+                            <ul>
+                                <li>Work with other developers using MVC architecture and OOP.</li>
+                                <li>Worked with other engineers to develop and implement a design system for the payment solution on the application.</li>
+                                <li>Unit tests, automation, and code reviews.</li>
+                            </ul>
+                        </section>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="ds-work-section">
+            <div class="container">
+                <h2 class="ds-heading">Latest works</h2>
+                <div class="ds-work-list-section">
+                    <div class="ds-work-list">
+                        <div class="row">
+                            <div class="col-12 col-sm-12 col-md-7 col-lg-7 col-xl-7 col-xxl-7">
+                                <section>
+                                    <h3 class="ds-work-tilte">PaySprint</h3>
+                                    <p>Safe and Secure Way to Send and Receive Money from over 100 Countries at no cost. Money Received Instantly, No hidden fees.</p><a href="https://paysprint.ca" class="ds-button">Details</a>
+                                </section>
+                            </div>
+                            <div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 col-xxl-5">
+                                <figure><img alt="workexperience" src="/portfolio/static/media/work-1.3e0fd59337075b454a21.jpg"></figure>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="ds-work-list-section">
+                    <div class="ds-work-list">
+                        <div class="row">
+                            <div class="col-12 col-sm-12 col-md-7 col-lg-7 col-xl-7 col-xxl-7">
+                                <section>
+                                    <h3 class="ds-work-tilte">AgroLink</h3>
+                                    <p>Safe and Secure way to connect farmers and custormers who are looking for a fresh farmer procduct directly from fram.</p><a href="https://paysprint.ca" class="ds-button">Details</a>
+                                </section>
+                            </div>
+                            <div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 col-xxl-5">
+                                <figure><img alt="workexperience" src="images/agrolink.png"></figure>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <footer class="ds-footer text-center">
+            <div class="container">
+                <section><span>Stay in touch</span>
+                    <h4>Ready to talk?</h4>
+                    <p>Feel free to contact us</p><a href="mailto:adesanyapelumi2017@gmail.com" class="ds-button">Lets Talk</a>
+                </section><span class="ds-copyright">© 2025 All rights reserved.</span>
+            </div>
+        </footer>
+
+
+
+
+
+
+    </div>
+</body>
+
+</html>
